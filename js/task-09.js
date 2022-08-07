7528cfe2464f06b8d82d2ee87d7763eec4bcf0f0
Bohdan Antoshkiv <Antoshkivbl@gmail.com>
2022-08-07 09:17:39 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -13,6 +13,21 @@
 //     .padStart(6, 0)}`;
 // }
 
+
+const refs = {
+  button: document.querySelector(".change-color"),
+  body: document.querySelector("body"),
+  colorName: document.querySelector(".color"),
+};
+
+refs.button.addEventListener("click", () => {
+  refs.button.addEventListener("click", getRandomHexColor);
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.colorName.textContent = getRandomHexColor();
+});
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
